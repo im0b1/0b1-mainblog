494551cf10a6ce8369d8985991f53f1c43e3ec1c
무뼈 <77718835+im0b1@users.noreply.github.com>
2025-05-02 20:02:30 +0900

    Update SearchInput.tsx

--- src/routes/Feed/SearchInput.tsx
+++ src/routes/Feed/SearchInput.tsx
@@ -8,7 +8,7 @@ const SearchInput: React.FC<Props> = ({ ...props }) => {
   return (
     <StyledWrapper>
       <div className="top">
-        <Emoji>🔎</Emoji> Search
+        검색
       </div>
       <input
         className="mid"
