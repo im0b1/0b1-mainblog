b777ae5d27308066f74e17908aea831ed26ff7b2
무뼈 <77718835+im0b1@users.noreply.github.com>
2025-05-05 23:24:24 +0900

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -4,7 +4,7 @@ const CONFIG = {
     name: "무뼈",
     image: "/avatar.png", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "invest & improve",
-    bio: "다양한 장르를 파고드는 폴리매스형 백수입니다. 뼈빠지게 일하기 싫어서 투자를 주로 연구합니다.",
+    bio: "다양한 장르를 파고드는 폴리매스형 백수. 뼈빠지게 일하기 싫어서 투자를 주로 연구합니다.",
     email: "kickthebone@gmail.com",
     linkedin: "",
     github: "im0b1",
