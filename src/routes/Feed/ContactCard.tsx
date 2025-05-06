8f8db7487fd38afde1ee970becb9de3170235de1
무뼈 <77718835+im0b1@users.noreply.github.com>
2025-05-06 14:45:26 +0900

    Update ContactCard.tsx

--- src/routes/Feed/ContactCard.tsx
+++ src/routes/Feed/ContactCard.tsx
@@ -44,7 +44,7 @@ const ContactCard: React.FC = () => {
             css={{ overflow: "hidden" }}
           >
             <AiOutlineMail className="icon" />
-            <div className="name">email</div>
+            <div className="name">kickthebone@gmail.com</div>
           </a>
         )}
         {CONFIG.profile.linkedin && (
